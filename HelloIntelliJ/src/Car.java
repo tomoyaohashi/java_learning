23ffe6e769e18dcd98f53e8c0c026dace85be00b
tomoyaohashi <t.ohashi1016@gmail.com>
2023-07-15 15:23:13 +0900

    カプセル化

--- HelloIntelliJ/src/Car.java
+++ HelloIntelliJ/src/Car.java
@@ -1,15 +1,41 @@
+/* speedフィールドがカプセル化と公開メソッドを用意したCarクラス */
 class Car {
     /* スピードを保存するフィールド */
-    int speed;
+    private int speed;
+
+    /* スピードを取得するメソッド */
+    int getSpeed() {
+        return this.speed;
+    }
 
     /* スピードを上げるメソッド */
     void speedUp(int value) {
+        if (value < 0) {
+            /* 負の加速は無視する */
+            return;
+        }
         this.speed += value;
+        /* 安全装置（最高速度）*/
+        if (this.speed >= 180) {
+            this.speed = 180;
+        }
     }
 
     /* スピードを下げるメソッド */
     void speedDown(int value) {
+        if (value < 0) {
+            /* 負の減速は無視する */
+            return;
+        }
         this.speed -= value;
+        /* 安全装置（バック時） */
+        if (this.speed <= -15) {
+            this.speed = -15;
+        }
     }
 }
-//メソッドの値が返す必要のないものであった場合は「void型」を使用する
+
+/*
+・メソッドの値が返す必要のないものであった場合は「void型」を使用する
+・get＋フィールド名（ゲッターメソッド）でカプセル化された現在の速度（変数）を取得できる
+ */
--- HelloIntelliJ/src/Main.java
+++ HelloIntelliJ/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        /* 引数に値を直接指定してspeedUpメソッドを呼び出す */
-        car.speedUp(10);
-        /* 引数に変数を指定してspeedUpメソッドを呼び出す */
-        int newSpeed = 80;
-        car.speedUp(newSpeed);
+        Car car;
+        car = new Car();
+        car.speedUp(100000);    /* 時速10万km加速しようとしても、180kmにとどまる */
+        car.speedDown(-40);     /* マイナスの減速は無視される　*/
+
+        /* 速度 = 180 */
+        System.out.println("速度 = " + car.getSpeed());
     }
 }
